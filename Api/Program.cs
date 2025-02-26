331bbf182533a0a73c80c61815e5807a1b0499aa
Rune Soerensen <rsoerensen@salesforce.com>
2025-02-26 14:31:07 -0500

    Implement Api sample

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,35 @@
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<IConnection>(sp =>
+{
+    var factory = new ConnectionFactory
+    {
+        Uri = new Uri(Environment.GetEnvironmentVariable("CLOUDAMQP_URL") ?? "amqp://guest:guest@localhost:5672")
+    };
+    return factory.CreateConnectionAsync().Result;
+});
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/send-email", async (HttpContext context, IConnection rabbitMqConnection) =>
+{
+    var emailRequest = await JsonSerializer.DeserializeAsync<EmailRequest>(context.Request.Body);
+    if (emailRequest == null)
+    {
+        return Results.BadRequest();
+    }
+
+    using var channel = await rabbitMqConnection.CreateChannelAsync();
+    await channel.QueueDeclareAsync(queue: "email_queue", durable: true, exclusive: false, autoDelete: false);
+
+    var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(emailRequest));
+    await channel.BasicPublishAsync("", "email_queue", body);
+
+    return Results.Accepted();
+});
 
 app.Run();
+
+record EmailRequest(string To, string Subject, string Body);
