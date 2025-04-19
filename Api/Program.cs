218b8f99adce2051bc0cbad7ff6d10c580fbf7db
Rune Soerensen <rsoerensen@salesforce.com>
2025-04-18 21:03:37 -0400

    Add basic sample email MCP tool

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,8 @@
+using System.ComponentModel;
 using System.Text;
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using ModelContextProtocol.Server;
 using RabbitMQ.Client;
 using Shared;
 using Shared.Messaging;
@@ -13,6 +15,7 @@ if (!string.IsNullOrEmpty(cloudAmqpUrl))
     builder.Configuration["ConnectionStrings:CloudAMQP"] = cloudAmqpUrl;
 }
 builder.Services.AddMessaging();
+builder.Services.AddMcpServer().WithHttpTransport().WithTools<SampleEmailTool>();
 var app = builder.Build();
 
 app.MapPost("/send-email", async (HttpContext context, RabbitMqService rabbitMqService, [FromBody] EmailRequest emailRequest) =>
@@ -23,4 +26,22 @@ app.MapPost("/send-email", async (HttpContext context, RabbitMqService rabbitMqS
     return Results.Accepted();
 });
 
+app.MapMcp();
 app.Run();
+
+[McpServerToolType]
+public sealed class SampleEmailTool
+{
+    [McpServerTool, Description("Send an email.")]
+    public static async Task<string> SendEmail(
+        RabbitMqService rabbitMqService,
+        [Description("The recipient email address")] string to,
+        [Description("The email subject")] string subject,
+        [Description("The email body")] string body)
+    {
+        var emailRequest = new EmailRequest(to, subject, body);
+        await rabbitMqService.PublishAsync(emailRequest);
+
+        return $"Email request queued {emailRequest}";
+    }
+}
